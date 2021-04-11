2c1c3f4997da9f5d690f54fb4d80c7e89b2a8161
rodrigo resque <rodrigo.resque@gmail.com>
2021-04-11 19:03:22 -0300

    refac!: add @before annotation

--- src/test/java/br/ce/wcaquino/servicos/LocacaoTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoTest.java
@@ -5,9 +5,7 @@ import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
 import br.ce.wcaquino.exceptions.LocacaoException;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
@@ -23,10 +21,17 @@ public class LocacaoTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
+    private LocacaoService locacaoService;
+
+    @Before
+    public void setup() {
+        locacaoService = new LocacaoService();
+    }
+
+
     @Test()
     public void deveValidarValores() throws Exception {
 
-        LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 2, 5.5);
 
@@ -49,7 +54,6 @@ public class LocacaoTest {
     @Test(expected = FilmeSemEstoqueException.class)
     public void checaFilmeSemEstoque1() throws Exception {
 
-        LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 0, 5.5);
 
@@ -59,7 +63,6 @@ public class LocacaoTest {
     @Test()
     public void checaFilmeSemEstoque2() throws Exception{
 
-        LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 0, 5.5);
 
@@ -76,7 +79,6 @@ public class LocacaoTest {
     @Test
     public void checaFilmeSemEstoqueLancandoExcecao() throws Exception {
 //      cenario
-        LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 0, 5.5);
 //      acao
@@ -89,11 +91,10 @@ public class LocacaoTest {
 
     @Test
     public void checaSeUsarioEstaVazio() throws FilmeSemEstoqueException {
-        LocacaoService service = new LocacaoService();
         Filme filme =  new Filme("Filme 1", 1, 5.5);
 
         try {
-            service.alugarFilme(null, filme);
+            locacaoService.alugarFilme(null, filme);
             Assert.fail();
         } catch (LocacaoException e) {
             assertThat(e.getMessage(), is(equalTo("Usuario nao pode ser nulo!")));
@@ -102,25 +103,23 @@ public class LocacaoTest {
 
     @Test
     public void chacaSeFimeEstaVazio() throws FilmeSemEstoqueException {
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
 
         try {
-            service.alugarFilme(usuario, null);
+            locacaoService.alugarFilme(usuario, null);
         }  catch (LocacaoException e) {
-            assertThat(e.getMessage(), is(equalTo("Filme nao pode ser nulo")));
+            assertThat(e.getMessage(), is(equalTo("Filme vazio")));
         }
     }
 
     @Test
     public void chacaSeFimeEstaVazio2() throws FilmeSemEstoqueException, LocacaoException {
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
 
         expectedException.expect(LocacaoException.class);
         expectedException.expectMessage("Filme vazio");
 
-        service.alugarFilme(usuario, null);
+        locacaoService.alugarFilme(usuario, null);
 
 
     }
