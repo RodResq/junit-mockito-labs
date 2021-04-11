7ea59ce738d34c5be2c36129402590dcf4aba0ea
rodrigo resque <rodrigo.resque@gmail.com>
2021-04-11 15:24:58 -0300

    feat!: usando errorColector;

--- src/test/java/br/ce/wcaquino/servicos/LocacaoTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoTest.java
@@ -4,14 +4,19 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ErrorCollector;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocacaoTest {
+
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
 
     @Test
     void deveValidarValores() {
@@ -25,14 +30,14 @@ public class LocacaoTest {
 
         //verificacao
 
-        assertThat(locacao.getValor(), is(equalTo(5.5)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-        assertEquals(locacao.getUsuario(), usuario);
-        assertEquals(locacao.getFilme(), filme);
-        assertEquals(locacao.getUsuario().getNome(), usuario.getNome());
-        assertEquals(locacao.getFilme().getNome(), filme.getNome());
-        assertEquals(locacao.getFilme().getEstoque(), filme.getEstoque());
-        assertEquals(locacao.getFilme().getPrecoLocacao(), filme.getPrecoLocacao());
+        errorCollector.checkThat(locacao.getValor(), is(equalTo(70.0)));
+        errorCollector.checkThat(locacao.getValor(), is(not(6.0)));
+        errorCollector.checkThat(locacao.getUsuario(), is(equalTo(usuario)));
+        errorCollector.checkThat(locacao.getFilme(), is(equalTo(filme)));
+        errorCollector.checkThat(locacao.getUsuario().getNome(), is(equalTo(usuario.getNome())));
+        errorCollector.checkThat(locacao.getFilme().getNome(), is(equalTo(filme.getNome())));
+        errorCollector.checkThat(locacao.getFilme().getEstoque(), is(equalTo(filme.getEstoque())));
+        errorCollector.checkThat(locacao.getFilme().getPrecoLocacao(), is(equalTo(filme.getPrecoLocacao())));
 
     }
 }
