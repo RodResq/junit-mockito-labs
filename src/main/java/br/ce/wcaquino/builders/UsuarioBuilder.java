d857addb6938e770fbef8a16911c7827403eea2a
rodrigo resque <rodrigo.resque@gmail.com>
2021-04-12 08:30:27 -0300

    feat!: criacao usuario builder!

--- src/main/java/br/ce/wcaquino/builders/UsuarioBuilder.java
+++ src/main/java/br/ce/wcaquino/builders/UsuarioBuilder.java
@@ -1,5 +1,22 @@
 package br.ce.wcaquino.builders;
 
+import br.ce.wcaquino.entidades.Usuario;
+
 public class UsuarioBuilder {
 
+    private Usuario usuario;
+
+    public UsuarioBuilder() {}
+
+    public static UsuarioBuilder umUsuario() {
+        UsuarioBuilder builder = new UsuarioBuilder();
+        builder.usuario = new Usuario();
+        builder.usuario.setNome("Usuario 1");
+
+        return builder;
+    }
+
+    public Usuario agora() {
+        return usuario;
+    }
 }
