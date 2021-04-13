f0ef2cac73a87fa14e51d63947c190519ab921d7
rodrigo resque <rodrigo.resque@gmail.com>
2021-04-12 23:39:19 -0300

    feat!: criacao classe DiaSemanaMatcher

---
+++ src/main/java/br/ce/wcaquino/suits/SuiteExecucao.java
@@ -0,0 +1,14 @@
+package br.ce.wcaquino.suits;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+//        CalculadoraTest.class,
+//        CalculoValorLocacaoTest.class,
+//        LocacaoTest,
+})
+public class SuiteExecucao {
+}
